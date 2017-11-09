8d2e00712d57492ee54804095132598066696493
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2017-11-09 22:29:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
-char win;
-char cells[9] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+	char win;
+	char cells[9] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
 
 
 void show_cells() {
@@ -15,91 +15,101 @@ void show_cells() {
 	cout << " +---+---+---+" << endl;
 }
 
-void show_available_moves() {
-	int s=0;
-	for (int i = 0; i<9; i++) {
-		if (i<3) {
-			cout << ++s << ". mark cell a" << i + 1 << " as";
-			//
+void show_available_moves(char op) {
+		
+	int k = 0;
+	for (int i = 0; i < 9; i++) {
+			if (!(cells[i] == 'X' || cells[i] == 'O')) {
+				if (i < 3) {
+					cout << ++k << ".mark cell a" << i + 1 << " as " << op << endl;
+				}
+				if (i>2 && i<6) {
+					cout << ++k << ".mark cell b" << i + 1 << " as " << op << endl;
+				}
+				if (i>5) {
+					cout << ++k << ".mark cell c" << i + 1 << " as " << op << endl;
+				
+			}
 		}
 	}
+	cout << ++k << ". quit" << endl;
 }
 
-void make_move(int num) {
-	int cell;
+void make_move( int cell) {
 	cin >> cell;
 	cout << endl;
-	while (cell>9 || cell<1 || cells[cell - 1] == 'O' || cells[cell - 1] == 'X') {
-		cout << " Enter a correct cells  number to make move";
-		cin >> cell;
-		cout << endl;
-
-
-	
-
-	}
-	if (num == 1) {
-		cells[cell - 1] ='X';
-		
-	}
-	else cells[cell - 1] = 'O';
-
-
-
-	
-}
-
-char check(){
-	for (int i = 0; i < 3; i++) { 
-
-		if ((cells[i * 3] == cells[i * 3 + 1] && cells[i * 3 + 1] == cells[i * 3 + 2]) && cells[i * 3]!=' '){ 
-
-			return cells [i];;
+		while (cin >> cell)
+		{
+			int k = 0;
+			for (int i = 0; i < 9; i++)
+			{
+				if (cells[i] == ' ') {
+					k++;
+				}
+				if (cells[i] == 'X') {
+					break;
+				}
+				if (cells[i] == 'O') {
+					break;
+				}
 			}
-
-		if ((cells[i] == cells[i + 3] && cells[i + 3] == cells[i + 6]) && cells[i]!=' '){ 
-		return cells [i];
-			}
-
-		if (((cells[2] == cells[4] && cells[4] == cells[6]) && cells[2]!=' ') || ((cells[0] == cells[4] && cells[4] == cells[8]) && cells[0]!=' ')){ 
-
-			return cells [i];
-			}
+		}
 	}
-	
-		return ' '; 
-	
+
+char check() {
+	for (int i = 0; i < 3; i++) {
+
+		if ((cells[i * 3] == cells[i * 3 + 1] && cells[i * 3 + 1] == cells[i * 3 + 2]) && cells[i * 3] != ' ') {
+
+			return cells[i];;
+		}
+
+		if ((cells[i] == cells[i + 3] && cells[i + 3] == cells[i + 6]) && cells[i] != ' ') {
+			return cells[i];
+		}
+
+		if (((cells[2] == cells[4] && cells[4] == cells[6]) && cells[2] != ' ') || ((cells[0] == cells[4] && cells[4] == cells[8]) && cells[0] != ' ')) {
+
+			return cells[i];
+		}
+	}
+
+	return ' ';
+
 }
 
 
 
 
 int main() {
+	char op;
 	show_cells();
-	show_available_moves();
 	for (int move = 1; move <= 9; move++) {
 		if (move % 2) {
+		   	op='X';
+		   	show_available_moves(op);
 			make_move(1);
 		}
-		else make_move(2);
+		else 	op='O';
+		show_available_moves(op);
+		make_move(2);
 		show_cells();
-		if (move >= 5) { 
-		check();
-			char  win = check(); 
-			    if (win == 'X') {
-		cout << "X win";
-	}
-	 if (win == 'O') {
-		cout << "O win";
-	}
-	if (win == ' ') {
-		cout << " nichja";
+		if (move >= 5) {
+			check();
+			char  win = check();
+			if (win == 'X') {
+				cout << "X win";
+			}
+			if (win == 'O') {
+				cout << "O win";
+			}
+			if (move==9 && (win!='O') && (win!='X')) {
+				cout << " nichja";
+			}
+			break;
 		}
-			    
-			    break;
-			    }
-		
-		}
-	
+
+	}
+
 	return 0;
 }
