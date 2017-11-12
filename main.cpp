f652822ce657eab542457981c0298f5efc10097c
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2017-11-12 18:54:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
+
+#include "stdafx.h"
 #include <iostream>
 using namespace std;
-	char win;
-	char cells[9] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+char win;
 
 
-void show_cells() {
+
+void show_cells(char cells[9]) {
 	cout << "   1   2   3" << endl;
 	cout << " +---+---+---+" << endl;
 	cout << "a| " << cells[0] << " | " << cells[1] << " | " << cells[2] << " |" << endl;
@@ -15,101 +17,103 @@ void show_cells() {
 	cout << " +---+---+---+" << endl;
 }
 
-void show_available_moves(char op) {
-		
+void show_available_moves(char op, char cells[9]) {
+
 	int k = 0;
 	for (int i = 0; i < 9; i++) {
-			if (!(cells[i] == 'X' || cells[i] == 'O')) {
-				if (i < 3) {
-					cout << ++k << ".mark cell a" << i + 1 << " as " << op << endl;
-				}
-				if (i>2 && i<6) {
-					cout << ++k << ".mark cell b" << i + 1 << " as " << op << endl;
-				}
-				if (i>5) {
-					cout << ++k << ".mark cell c" << i + 1 << " as " << op << endl;
-				
+		if (!(cells[i] == 'X' || cells[i] == 'O')) {
+			if (i < 3) {
+				cout << ++k << ".mark cell a" << i + 1 << " as " << op << endl;
+			}
+			if (i>2 && i<6) {
+				cout << ++k << ".mark cell b" << i + 1 << " as " << op << endl;
+			}
+			if (i>5) {
+				cout << ++k << ".mark cell c" << i + 1 << " as " << op << endl;
+
 			}
 		}
 	}
 	cout << ++k << ". quit" << endl;
 }
 
-void make_move( int cell) {
-	cin >> cell;
-	cout << endl;
-		while (cin >> cell)
-		{
-			int k = 0;
-			for (int i = 0; i < 9; i++)
-			{
-				if (cells[i] == ' ') {
-					k++;
-				}
-				if (cells[i] == 'X') {
-					break;
-				}
-				if (cells[i] == 'O') {
-					break;
-				}
-			}
+int make_move(char op, char cells[9]){
+int cell;
+while (cin >> cell)
+{
+	int k = 0;
+	for (int i = 0; i < 9; i++)
+	{
+		if (cells[i] == ' ') {
+			k++;
+		}
+		if (k == cell) {
+			cells[i] = op;
+			show_cells(cells);
+			return 0;
 		}
 	}
-
-char check() {
-	for (int i = 0; i < 3; i++) {
-
-		if ((cells[i * 3] == cells[i * 3 + 1] && cells[i * 3 + 1] == cells[i * 3 + 2]) && cells[i * 3] != ' ') {
-
-			return cells[i];;
-		}
-
-		if ((cells[i] == cells[i + 3] && cells[i + 3] == cells[i + 6]) && cells[i] != ' ') {
-			return cells[i];
-		}
-
-		if (((cells[2] == cells[4] && cells[4] == cells[6]) && cells[2] != ' ') || ((cells[0] == cells[4] && cells[4] == cells[8]) && cells[0] != ' ')) {
-
-			return cells[i];
-		}
-	}
-
-	return ' ';
-
 }
 
+	}
 
+	char check(char cells[9]) {
+		for (int i = 0; i < 3; i++) {
 
+			if ((cells[i * 3] == cells[i * 3 + 1] && cells[i * 3 + 1] == cells[i * 3 + 2]) && cells[i * 3] != ' ') {
 
-int main() {
-	char op;
-	show_cells();
-	for (int move = 1; move <= 9; move++) {
-		if (move % 2) {
-		   	op='X';
-		   	show_available_moves(op);
-			make_move(1);
+				return cells[i];;
+			}
+
+			if ((cells[i] == cells[i + 3] && cells[i + 3] == cells[i + 6]) && cells[i] != ' ') {
+				return cells[i];
+			}
+
+			if (((cells[2] == cells[4] && cells[4] == cells[6]) && cells[2] != ' ') || ((cells[0] == cells[4] && cells[4] == cells[8]) && cells[0] != ' ')) {
+
+				return cells[i];
+			}
 		}
-		else 	op='O';
-		show_available_moves(op);
-		make_move(2);
-		show_cells();
-		if (move >= 5) {
-			check();
-			char  win = check();
-			if (win == 'X') {
-				cout << "X win";
-			}
-			if (win == 'O') {
-				cout << "O win";
-			}
-			if (move==9 && (win!='O') && (win!='X')) {
-				cout << " nichja";
-			}
-			break;
-		}
+
+		return ' ';
 
 	}
 
-	return 0;
-}
+
+
+
+	int main() {
+		char cells[9] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+		char op;
+		show_cells(cells);
+		for (int move = 1; move <= 9; move++) {
+			if (move % 2) {
+				op = 'X';
+				show_available_moves(op, cells);
+				make_move(1, cells);
+			}
+			else {
+				op = 'O';
+				show_available_moves(op, cells);
+				make_move(2, cells);
+			}
+			if (move >= 5) {
+				check(cells);
+				char  win = check(cells);
+				if (win == 'X') {
+					cout << "X win";
+				}
+				if (win == 'O') {
+					cout << "O win";
+				}
+				if (move == 9 && (win != 'O') && (win != 'X')) {
+					cout << " nichja";
+				}
+				break;
+			}
+
+		}
+
+		return 0;
+	}
+
